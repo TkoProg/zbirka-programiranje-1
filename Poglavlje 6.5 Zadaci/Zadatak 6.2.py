ac71e9d92446888d6fe27c95f4cd37254b793c09
tamiroladejo <koltam09@gmail.com>
2023-11-29 15:00:31 +0100

    Zadatak 6.2 uradjen.

---
+++ Poglavlje 6.5 Zadaci/Zadatak 6.2.py
@@ -0,0 +1,13 @@
+def provjeraPonavljanja(n,m):
+    n = str(n)
+    m = str(m)
+    if m in n:
+        return "Nalazi se"
+    else:
+        return "Ne nalazi se"
+
+
+n = int(input())
+m = int(input())
+
+print(provjeraPonavljanja(n,m))
