896581ea5c9119c18a46902777accb7b70024958
tamiroladejo <koltam09@gmail.com>
2023-11-30 13:54:24 +0100

    Zadatak 7.3 uradjen.

---
+++ Poglavlje 7.1 Zadaci/Zadatak 7.3.py
@@ -0,0 +1,19 @@
+brojevi = []
+
+m = int(input("Unesite paran broj m: "))
+
+for i in range(m):
+    n = int(input(f"Unesite {i}. cijeli broj: "))
+    brojevi.append(n)
+
+najmanji = 0
+najveci = 0
+brojevi = sorted(brojevi)
+
+for i in range(len(brojevi)):
+    if i < len(brojevi) / 2:
+        najmanji += brojevi[i]
+    else:
+        najveci += brojevi[i]
+
+print(f"Vas rezultat je: {najveci-najmanji}")
