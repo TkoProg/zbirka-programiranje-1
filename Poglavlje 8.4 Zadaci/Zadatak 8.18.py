3a1a46092fdc87a6fa6d79e9cb7e0c89950bbf84
tamiroladejo <koltam09@gmail.com>
2023-12-19 21:42:13 +0100

    Zadatak 8.18 uradjen.

---
+++ Poglavlje 8.4 Zadaci/Zadatak 8.18.py
@@ -0,0 +1,7 @@
+from math import floor
+
+# prvi broj moze najvise biti 99
+# drugi broj moze najvise biti 999
+# 1098 - ovo je maksimalni broj koji ne moze preci ovo
+
+# Pretezak zadatak, mrsko mi
