b4869943d30e52ff3c1a4519a6f004dfae05409f
tamiroladejo <koltam09@gmail.com>
2023-12-19 01:38:10 +0100

    Zadatak 7.28 uradjen.

---
+++ Poglavlje 7.5 Zadaci/Zadatak 7.28.py
@@ -0,0 +1,10 @@
+# n = input().split()
+#
+# brojevi = [int(x) for x in n]
+# najveca_suma = 0
+#
+# for i in range(len(brojevi)):
+#     suma = 0
+#     for j in range(i, len(brojevi)):
+
+# JA STVARNO NE RAUZMIJEM STA SE TRAZI U OVOM ZADATKU I QUITE FRANKLY NE ZANIMA ME
